61f6c3810364bbbb0fbcac79771ecdc8290d5225
Guillaume Legrand <legrand.guillaume.frantz@orange.fr>
2019-03-29 21:13:19 +0100

    JS

---
+++ BetterFuckingWebsite/scripts/main.js
@@ -0,0 +1,9 @@
+setTimeout(MyFunction, 10000);
+
+function MyFunction(){
+	alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please');
+}
+
+function changeBGC(color){
+document.bgColor = '#0984e3';
+}
